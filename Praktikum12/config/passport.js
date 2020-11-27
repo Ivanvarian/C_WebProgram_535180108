529012f064079798bc371b286a8e22cf2e50533a
Ivanvariann <71561796+Ivanvariann@users.noreply.github.com>
2020-11-28 03:11:05 +0700

    commit

---
+++ Praktikum12/config/passport.js
@@ -0,0 +1,34 @@
+const LocalStrategy = require("passport-local").Strategy;
+
+const mongoose = require("mongoose");
+
+const bcrypt = require("bcryptjs");
+
+//user model
+const user = require("../models/User")
+
+module.exports = function (passport) {
+    passport.use(
+        new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+            //user cocok
+            User.findOne({ email: email })
+            .then ((user) => {
+                if (!user) {
+                    return done(null, false, { message: "email tidak terdaftar"});
+                }
+
+            //cek password 
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) throw err;
+
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, { message: "password salah"});
+                }
+              });
+            })
+            .catch((err) => console.log(err));
+        })
+    );
+};
